ad75a50582f834cd757191e43e3c5d345f4069e9
Samuli Ulmanen <samuli.ulmanen@wunderdog.fi>
2023-12-17 08:49:57 +0200

    save status

--- 10/area.ex
+++ 10/area.ex
@@ -1,9 +1,15 @@
 import Enum
 defmodule Area do
-  def inside?(pipes, point) do
-    point_x = elem(point, 0)
-    point_y = elem(point, 1)
-    point_x > min_x(pipes, point_y) and point_x < max_x(pipes, point_y) and point_y > min_y(pipes, point_x) and point_y < max_y(pipes, point_x)
+  def has_y?(pipes, y) do
+    pipes
+    |> filter(fn(pipe_point) -> elem(pipe_point, 1) == y end)
+    |> any?
+  end
+
+  def has_x?(pipes, x) do
+    pipes
+    |> filter(fn(pipe_point) -> elem(pipe_point, 0) == x end)
+    |> any?
   end
 
   def min_x(pipes, y) do
@@ -28,5 +34,11 @@ defmodule Area do
     pipes
     |> filter(fn(pipe_point) -> elem(pipe_point, 0) == x end)
     |> min
+  end
+
+  def inside?(pipes, point) do
+    point_x = elem(point, 0)
+    point_y = elem(point, 1)
+    has_y?(pipes, point_y) and (point_x > min_x(pipes, point_y)) and (point_x < max_x(pipes, point_y)) and has_x?(pipes, point_x) and (point_y > min_y(pipes, point_x)) and (point_y < max_y(pipes, point_x))
   end
 end
--- 10/solve.exs
+++ 10/solve.exs
@@ -1,17 +1,28 @@
 import Escape
+import Area
 import Enum
 
 {:ok, input} = File.read("input.txt")
 
 matrix = matrix(input)
-
-start_node = start(index(matrix))
+matrix_index = index(matrix)
+start_node = start(matrix_index)
 
 start_possible = possible(matrix, start_node, "S", [start_node])
 back = at(start_possible, 1)
 forward = at(start_possible, 0)
 
-escape(matrix, forward, back, [start_node, forward], [start_node, back])
+pipes = escape(matrix, forward, back, [start_node, forward], [start_node, back])
+
+IO.puts("Part 1:")
+pipes
 |> map(fn(walked) -> count(walked) end)
 |> max
+|> IO.puts
+
+all_pipes = reduce(pipes, [], fn x, acc -> concat(acc, x) end)
+
+IO.puts("Part 2:")
+matrix_index
+|> reduce(0, fn(line, acc) -> reduce(elem(line, 0), acc, fn(point, acc) -> if inside?(all_pipes, {elem(point, 1), elem(line, 1)}), do: acc + 1, else: acc end) end)
 |> IO.puts
